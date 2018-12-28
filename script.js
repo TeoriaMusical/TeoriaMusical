89b202eaac217c1e1bf6ade12fb89d806eef36d6
Tiago <tiagosn03@gmail.com>
2018-12-28 15:55:46 -0300

    Adicionei um interface gráfica

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 import chordFactory from "./chords/ChordFactory.js"
 
-let chordTitle = document.querySelector('#chord');
-let chordName = 'B';
-let chord = chordFactory(chordName);
+//              DOM Objects                 //
+let chordTitle  = document.querySelector('#chord');        // Título
+let chordTriade = document.querySelector('#triade');       // Tríade
+let buttons     = document.querySelectorAll('.btn');       // Botões
 
-chordTitle.innerHTML += " " + chordName;
+//              Variables                   //
+let chordChoosedByUser = "";
+
+//              Buttons events              //
+buttons.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        
+        chordChoosedByUser = btn.innerHTML;
+        UpdateView();
+    
+    });
+});
 
 
-let chordTriade = document.querySelector('#triade');
-chordTriade.innerHTML += chord.tonic + " - " + chord.third + " - " + chord.fifth;
+function UpdateView() {
+
+    let chord = chordFactory(chordChoosedByUser);
+
+    chordTitle.innerHTML = "Acorde " + chordChoosedByUser;
+
+    chordTriade.innerHTML = chord.tonic + " - " + chord.third + " - " + chord.fifth;
+
+}
+
+
