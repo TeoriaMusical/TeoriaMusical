c4cb9207c8d1a66120758333d0b0e44cc46aaea8
Tiago <tiagosn03@gmail.com>
2019-01-23 15:33:59 -0300

    Exclui toda a feature do exercício. Devo refazê-lo todo novamente.

--- util/randomChordChooser.js
+++
@@ -1,5 +0,0 @@
-import randomNumber from "./randomNumber.js"
-
-export default function randomChordChooser(chordArray) {
-    return chordArray[randomNumber(0, chordArray.length)];
-}
--- util/randomNumber.js
+++
@@ -1,6 +0,0 @@
-export default function randomNumber(min_value, max_value) {
-
-    let random_Number = Math.random() * (max_value - min_value) + min_value;
-
-    return Math.floor(random_Number);
-}
